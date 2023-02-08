a8af69f42f9c4aa9769045fa37b5d59e5159be09
Jacobo <jacobo@fixel.co.jp>
2023-02-08 17:13:59 +0900

    client initial setup

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import { Header } from './components/Header';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className='container'>Hello</div>
+      </ApolloProvider>
+    </>
+  );
+}
+
+export default App;
