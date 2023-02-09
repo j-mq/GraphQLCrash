14bf2a92d9715a87c7f431e0257ba97f3a90b568
Jacobo <jacobo@fixel.co.jp>
2023-02-09 16:17:07 +0900

    mutation for deleting clients

--- client/src/components/ClientRow.tsx
+++ client/src/components/ClientRow.tsx
@@ -1,4 +1,7 @@
 import { FaTrash } from 'react-icons/fa';
+import { useMutation } from '@apollo/client';
+import { DELETE_CLIENT } from '../mutations/clientMutations';
+import { GET_CLIENTS } from '../queries/clientQueries';
 
 type ClientRowProps = {
   client: {
@@ -10,13 +13,34 @@ type ClientRowProps = {
 };
 
 const ClientRow = ({ client }: ClientRowProps) => {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: {
+      id: client.id,
+    },
+    // refetchQueries: [{ query: GET_CLIENTS }],
+    update(cache, { data: { deleteClient } }) {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS }) as any;
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: {
+          clients: clients.filter(
+            (client: any) => client.id !== deleteClient.id
+          ),
+        },
+      });
+    },
+  });
+
   return (
     <tr key={client.id}>
       <td>{client.name}</td>
       <td>{client.email}</td>
       <td>{client.phone}</td>
       <td>
-        <button className='btn btn-sm btn-danger'>
+        <button
+          className='btn btn-sm btn-danger'
+          onClick={() => deleteClient()}
+        >
           <FaTrash />
         </button>
       </td>
